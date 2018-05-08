38a7a73a089e8657266b01dbd677e41ed59bb412
Tomislav Tustonić <ttustonic@outlook.com>
2018-05-08 21:06:20 +0200

    nuget

--- Example/Program.cs
+++ Example/Program.cs
@@ -13,26 +13,7 @@ namespace Examples
 
         static void Main(string[] args)
         {
-//            Examples.SimpleExample();
-            var file= Path.Combine(_dir, "regression", "test.csv");
-
-            var dt = new DataTable();
-            using (var rdr = new CsvDataReader(file, true))
-            {
-//                rdr.ColumnTypes = new Type[] { typeof(int), typeof(string), typeof(int) };
-                dt.Load(rdr);
-            }
-
-            foreach (DataRow r in dt.Rows)
-            {
-                for (int i = 0; i < dt.Columns.Count; i++)
-                {
-                    var col = dt.Columns[i];
-                    Console.Write($"{dt.Columns[i]}:{r[i]}  ");
-                }
-                Console.WriteLine("");
-            }
-
+            Examples.SimpleExample();
         }
     }
 }
